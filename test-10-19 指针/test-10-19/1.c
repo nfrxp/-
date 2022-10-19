0f8dcc8c187e1a55bb0c2c10e565b5112d0a792c
小冉 <rjy716@qq.com>
2022-10-19 21:22:37 +0800

    代码

---
+++ test-10-19 指针/test-10-19/1.c
@@ -0,0 +1,88 @@
+//#include<stdio.h>
+//int main()
+//{
+//	char a[] = "\72";
+//	printf("占%d个字节\n", sizeof(a));
+//	return 0;
+//}
+
+//#include<stdio.h>
+//#include<string.h>
+//int main()
+//{
+//	char a[] = "478\345\\345\t\128\x15";
+//	printf("长度为%d 大小为%d字节\n", strlen(a), sizeof(a));
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//#include<string.h>
+//int main()
+//{
+//	char a[] = "\t\\x43\102\bcd";
+//	printf("长度为%d 大小为%d字节\n", strlen(a), sizeof(a));
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 1;
+//	double j = 2;
+//	double k = i / j;
+//	printf("k=%f", k);
+//
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 'a' + 10 + '\101' + 010 + 0x10;
+//	printf("%d\n", i);
+//	return 0;
+//}
+//
+//#include<stdio.h>
+//int main()
+//{
+//	char a = 250;
+//	printf("%c", a);
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//
+//int main()
+//{
+//	char a = 0xb6; 
+//	short b = 0xb600;
+//	int c = 0xb6000000;
+//	if (a == 0xb6)
+//		printf("a");
+//	if (b == 0xb600)
+//		printf("b");
+//	if (c == 0xb6000000)
+//		printf("c");
+//	return 0;
+//}
+
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//
+//int MAX(int x, int y)
+//{
+//	return x > y ? x : y;
+//}
+//int main()
+//{
+//	int x = 0;
+//	int y = 0;
+//	scanf("%d %d", &x, &y);
+//	int max = MAX(x, y);
+//	printf("%d", max);
+//	return 0;
+//}
+
---
+++ test-10-19 指针/test-10-19/指针.c
@@ -0,0 +1,23 @@
+//#include<stdio.h>			//野指针
+//
+//int test()
+//{
+//	int i = 4;
+//	return &i;			//局部变量
+//}
+//
+//int main()
+//{
+//	int* p = test();
+//	*p = 20;
+//
+//	return 0;
+//}
+
+
+#include<stdio.h>
+int main()
+{
+
+	return 0;
+}
