410549e3fd175ac740f233ccf64e3d22e56701e1
小冉 <rjy716@qq.com>
2022-10-26 22:21:56 +0800

    代码

--- test-10-26 类 继承 多态/test-10-26 类 继承 多态/多态.cpp
+++ test-10-26 类 继承 多态/test-10-26 类 继承 多态/多态.cpp
@@ -215,49 +215,230 @@
 //}
 
 
-#include<iostream>										//纯虚函数，这个类称为抽象类
+//#include<iostream>										//纯虚函数，这个类称为抽象类
+//using namespace std;
+//
+//class Base
+//{
+//public:
+//	virtual void fun() = 0;
+//	//只要有一个纯虚函数，该类就是抽象类
+//	// 抽象类的特点
+//	// 1、无法实例化对象
+//	// 2、抽象类的子类，必须重写父类中的纯虚函数，否则也属于抽象类
+//	//
+//};
+//
+//class Son:public Base
+//{
+//public:
+//
+//};
+//
+//class Son2 :public Base
+//{
+//public:
+//	void fun()
+//	{
+//		cout << "已对纯虚函数进行重写\n";
+//	}
+//};
+//
+//void test01()
+//{
+//	////1、无法实例化对象
+//	//Base a;
+//	//new Base;
+//
+//	////2、抽象类的子类必须重写纯虚函数，否则也是抽象类
+//	//Son a;
+//
+//	Base* p = new Son2;
+//	p->fun();
+//}
+//int main()
+//{
+//	test01(); 
+//	return 0;
+//}
+
+
+
+//#include<iostream>									//多态 制作饮品
+//using namespace std;
+//
+//class AbstractDrink
+//{
+//public:
+//	//烧水
+//	virtual void water() = 0;
+//	//冲泡
+//	virtual void pour() = 0;
+//	//分杯
+//	virtual void separate() = 0;
+//	//加辅料
+//	virtual void add() = 0;
+//
+//	//一套流程
+//	void func()
+//	{
+//		water();
+//		pour();
+//		separate();
+//		add();
+//	}
+//};
+//
+////泡茶
+//class Tea:public AbstractDrink
+//{
+//public:
+//	//烧水
+//	virtual void water()
+//	{
+//		cout << "烧开水\n";
+//	}
+//	//冲泡
+//	virtual void pour()
+//	{
+//		cout << "泡茶叶\n";
+//	}
+//	//分杯
+//	virtual void separate()
+//	{
+//		cout << "分杯\n";
+//	}
+//	//加辅料
+//	virtual void add()
+//	{
+//		cout << "加枸杞\n";
+//	}
+//
+//	//一套流程
+//	void func()
+//	{
+//		water();
+//		pour();
+//		separate();
+//		add();
+//	}
+//};
+//
+////制作咖啡
+//class Coffee :public AbstractDrink
+//{
+//public:
+//	//烧水
+//	virtual void water()
+//	{
+//		cout << "烧水\n";
+//	}
+//	//冲泡
+//	virtual void pour()
+//	{
+//		cout << "泡咖啡粉\n";
+//	}
+//	//分杯
+//	virtual void separate()
+//	{
+//		cout << "分杯\n";
+//	}
+//	//加辅料
+//	virtual void add()
+//	{
+//		cout << "加牛奶\n";
+//	}
+//
+//	//一套流程
+//	void func()
+//	{
+//		water();
+//		pour();
+//		separate();
+//		add();
+//	}
+//};
+//
+//void doWork(AbstractDrink* a)
+//{
+//	a->func();
+//	delete a;
+//}
+//
+//void test01()
+//{
+//	doWork(new Tea);
+//	cout << "------------" << endl;
+//	doWork(new Coffee);
+//}
+//int main()
+//{
+//	test01();
+//	return 0;
+//}
+
+
+
+#include<iostream>									//堆区数据	虚析构和纯虚析构
 using namespace std;
 
-class Base
-{
+class Animal {
 public:
-	virtual void fun() = 0;
-	//只要有一个纯虚函数，该类就是抽象类
-	// 抽象类的特点
-	// 1、无法实例化对象
-	// 2、抽象类的子类，必须重写父类中的纯虚函数，否则也属于抽象类
-	//
-};
-
-class Son:public Base
-{
-public:
-
-};
-
-class Son2 :public Base
-{
-public:
-	void fun()
+	Animal()
 	{
-		cout << "已对纯虚函数进行重写\n";
+		cout << "Animal 构造函数调用\n";
 	}
+
+	virtual void Speak() = 0;
+
+	//利用虚析构，解决父类指针释放子类对象不干净
+	//virtual ~Animal() {
+	//	cout << "Animal 虚构函数调用\n";
+	//}
+
+	//纯虚析构函数必须实现
+	//virtual ~Animal()  = 0;
+	virtual ~Animal() = 0;
+
+};
+Animal::~Animal()
+{
+	cout << "Animal 纯虚析构调用\n";
+}
+
+class Cat:public Animal
+{
+public:
+	Cat(string name)
+	{
+		cout << "Cat 构造函数调用\n";
+		Name = new string(name);
+	}
+	void Speak()
+	{
+		cout <<*Name<< "小猫在说话\n";
+	}
+
+	~Cat()
+	{
+		cout << "Cat 析构函数调用\n";
+		if (Name != NULL)
+		{
+			delete Name;
+			Name = NULL;
+		}
+	}
+	string* Name;
 };
 
 void test01()
 {
-	////1、无法实例化对象
-	//Base a;
-	//new Base;
-
-	////2、抽象类的子类必须重写纯虚函数，否则也是抽象类
-	//Son a;
-
-	Base* p = new Son2;
-	p->fun();
+	Animal* a = new Cat("Tom");
+	a->Speak();
+	delete a;
 }
 int main()
 {
-	test01(); 
+	test01();
 	return 0;
 }
