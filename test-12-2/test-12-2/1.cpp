8fe98fe948f8d61d7726e57fcde1ddb2244d3973
小冉 <rjy716@qq.com>
2022-12-02 22:40:38 +0800

    代码

--- test-12-2/test-12-2/1.cpp
+++ test-12-2/test-12-2/1.cpp
@@ -16,43 +16,75 @@
 //	return 0;
 //}
 
+
+
+//#include<iostream>
+//using namespace std;
+//
+////void test01() {
+////	//const char *a = "china";
+////	char* a;
+////	*a = "china";
+////
+////}
+//
+////void test02() {
+////	//char str[10] = { "china" };
+////
+////	char str[10];
+////	str[] = {"china"};	//左边非表达式
+////	cout << str << endl;
+////}
+//
+////void test03() {
+////	//const char* s = "china";
+////	const char* s;
+////	s = "china";
+////
+////}
+//
+////void test04() {
+////	//char c[4] = "abc", d[4] = "abc";
+////	char c[4] = d[4] = "abc";
+////}
+//int main() {
+//	//test01();
+//
+//	//test02();
+//
+//	//test03();
+//
+//	//test04();
+//
+//	return 0;
+//}
+
+#define _CRT_SECURE_NO_WARNINGS 1
 #include<iostream>
+#include<string.h>
 using namespace std;
 
-//void test01() {
-//	//const char *a = "china";
-//	char* a;
-//	*a = "china";
-//
-//}
+void test01() {
+	char a1[] = "abc", a2[80] = "1234";
+	strcat(a2, a1);
+	cout << a2 << endl;
+}
 
-//void test02() {
-//	//char str[10] = { "china" };
-//
-//	char str[10];
-//	str[] = {"china"};	//左边非表达式
-//	cout << str << endl;
-//}
+void test02() {
+	const char* aa[2] = { "abcd","ABCD" };
 
-//void test03() {
-//	//const char* s = "china";
-//	const char* s;
-//	s = "china";
-//
-//}
+}
 
-//void test04() {
-//	//char c[4] = "abc", d[4] = "abc";
-//	char c[4] = d[4] = "abc";
-//}
+void test03() {
+	int a[3][4] = { 0 };
+
+}
 int main() {
+
 	//test01();
 
 	//test02();
 
-	//test03();
-
-	//test04();
-
+	test03();
 	return 0;
 }
