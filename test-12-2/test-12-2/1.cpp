acb98baa5c5ab5e4b26d190c47a95d33b4a80942
小冉 <rjy716@qq.com>
2022-12-02 22:30:30 +0800

    代码

---
+++ test-12-2/test-12-2/1.cpp
@@ -0,0 +1,58 @@
+//#define SQR(x) (x)*x
+//#include<iostream>
+//using namespace std;
+//int main() {
+//	int k = 2;
+//	cout << SQR(k + 1);
+//	return 0;
+//}
+
+//#include<iostream>
+//using namespace std;
+//int main() {
+//	int a = 12;
+//	a += a -= a * a;
+//	cout << a << endl;
+//	return 0;
+//}
+
+#include<iostream>
+using namespace std;
+
+//void test01() {
+//	//const char *a = "china";
+//	char* a;
+//	*a = "china";
+//
+//}
+
+//void test02() {
+//	//char str[10] = { "china" };
+//
+//	char str[10];
+//	str[] = {"china"};	//左边非表达式
+//	cout << str << endl;
+//}
+
+//void test03() {
+//	//const char* s = "china";
+//	const char* s;
+//	s = "china";
+//
+//}
+
+//void test04() {
+//	//char c[4] = "abc", d[4] = "abc";
+//	char c[4] = d[4] = "abc";
+//}
+int main() {
+	//test01();
+
+	//test02();
+
+	//test03();
+
+	//test04();
+
+	return 0;
+}
