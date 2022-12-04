bf5848c41c0eb887d769ead165ae0030d5db8ef3
小冉 <rjy716@qq.com>
2022-12-04 21:52:27 +0800

    代码

--- test-12-4/test-12-4/1.cpp
+++ test-12-4/test-12-4/1.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<fstream>
 using namespace std;
 
 class Student {
@@ -17,6 +18,10 @@ public:
 	void show1() {
 		cout << ID << '\t' << Name << '\t' << Year << '\t' << Gender << '\t';
 	}
+	long long show_ID() { return ID; }
+	string show_Name() { return Name; }
+	int show_Year() { return Year; }
+	char show_Gender() { return Gender; }
 	~Student() {
 		cout << "调用基类析构函数！\n";
 	}
@@ -50,7 +55,14 @@ public:
 			<< "\t" << Score[3] << "\t" << Sum
 			<< "\t" << Average << "\n";
 	}
-
+	string show_Subject() { return Subject; }
+	float show_Score1() { return Score[0]; }
+	float show_Score2() { return Score[1]; }
+	float show_Score3() { return Score[2]; }
+	float show_Score4() { return Score[3]; }
+	float show_Sum() { return Sum; }
+	float show_Average() { return Average; }
+	
 	~Derived() {
 		cout << "调用派生类析构函数！\n";
 	}
@@ -82,6 +94,7 @@ int main() {
 	const int n = 10;
 	Derived arr[n];
 	int i = 0;
+	//依次输入十位学生的信息
 	for (i = 0; i < n; i++) {
 		cout << "————————————————\n";
 		cout << "请输入第" << i + 1 << "位同学的信息：\n";
@@ -94,6 +107,7 @@ int main() {
 		arr[i].get_Sum();
 		arr[i].get_Average();
 	}
+	//排序前
 	cout << "学号\t\t姓名\t年龄\t性别\t专业\t分数1\t分数2\t分数3\t分数4\t总分\t平均分\n";
 	for (i = 0; i < n; i++) {
 		arr[i].show1();
@@ -106,5 +120,20 @@ int main() {
 		arr[i].show1();
 		arr[i].show2();
 	}
+
+	ofstream ofs;
+	ofs.open("./7218.bat", ios::out);
+	if (!ofs.is_open()) {
+		cout << "文件打开失败！\n";
+		exit;
+	}
+	//写文件
+	for (i = 0; i < n; i++) {
+		ofs << arr[i].show_ID() <<" " << arr[i].show_Name() << " " << arr[i].show_Year() << " " <<
+			arr[i].show_Subject() << " " << arr[i].show_Score1() << " " << arr[i].show_Score2() << " " <<
+			arr[i].show_Score3() << " " << arr[i].show_Score4() << " " << arr[i].show_Sum() << " " <<
+			arr[i].show_Average() << endl;
+	}
+
 	return 0;
 }
