ba0869863b1729b6ea20a639089c79f91ae878e7
小冉 <rjy716@qq.com>
2022-12-09 17:01:38 +0800

    代码

--- test-12-9/test-12-9/1.cpp
+++ test-12-9/test-12-9/1.cpp
@@ -189,45 +189,190 @@
 //}
 
 
+//#include <iostream>
+//#include <iomanip>
+//#include <fstream>
+//#include <cstdlib>
+//#include <cmath>
+//
+//using namespace std;
+//
+//void fun(double*, int);
+//
+//void fun(double* p, int n)
+//{
+//    /***********************program*************************/
+//
+//    int i, j;
+//    double temp;
+//    for (i = 0; i < n-1; i++) {
+//        for (j = i + 1; j < n; j++) {
+//            if ((int)fabs(p[i]) / 10 % 1000 < (int)fabs(p[j]) / 10 % 1000) {
+//                temp = p[i]; p[i] = p[j]; p[j] = temp;
+//            }
+//        }
+//    }
+//    /*************************END***************************/
+//}
+//
+//int main(void)
+//{
+//    double a[10] = { -27888.52, 22177.83, 12496.14, -867.17, 29506.73, -18027.43, 32440.25, 91876.24,-18942.8, 10262.27 };
+//    int i;
+//    fun(a, 10);
+//
+//    cout << "排序结果：";
+//    cout << fixed << setprecision(2);
+//    for (i = 0; i < 10; i++) {
+//        cout << setw(10) << a[i];
+//    }
+//    cout << endl;
+//    
+//    return 0;
+//
+//}
+
+
+//#include<iostream>
+//using namespace std;
+//
+//int func(char *a,int b) {
+//	int sum = 0;
+//	while (*a) {
+//		if (*a > '0' && *a < '0' + b) {
+//			sum = sum * b + *a - '0';
+//		}
+//		else {
+//			return -1;
+//		}
+//		a++;
+//	}
+//	return sum;
+//}
+//int main() {
+//	char a[10] = "1425324";
+//	cin.getline(a, 10);
+//	int b = 6;
+//	cin >> b;
+//	int c=func(a,b);
+//	cout << "转换为十进制" << c << endl;//没有写完整，返回-1代表字符与进制不匹配
+//	return 0;
+//}
+
+//#include <iostream>
+//using namespace std;
+//
+//class exam
+//{
+//    int a;
+//public:
+//    exam(int i = 0)   {
+//        a = i;  cout << "constructor " << a << endl;
+//    }
+//    ~exam()   {
+//        cout << "deconstructor " << a << endl;
+//    }
+//    void print()
+//    {
+//        cout << a << endl;
+//    }
+//};
+//
+//void main()
+//{
+//    exam ob;
+//    ob = 4;
+//    ob.print();
+//    ob = exam(8);
+//    ob.print();
+//}
+
+
+//#include <iostream>
+//using namespace std;
+//
+//class exam
+//{
+//    int a;
+//    static int sum;
+//public:
+//    exam(int i = 0)   {
+//        a = i;  sum += a;
+//    }
+//    ~exam()   {
+//        sum -= a;
+//    }
+//    void print()   {
+//        cout << a << '\t' << sum << endl;
+//    }
+//};
+//
+//int exam::sum = 0;
+//
+//void main()
+//{
+//    exam ob1(6);
+//    ob1.print();
+//    exam ob2(7);
+//    ob2.print();
+//    exam ob3(10);
+//    ob3.print();
+//    ob1.~exam();
+//    ob3.print();
+//}
+
+
 #include <iostream>
-#include <iomanip>
-#include <fstream>
-#include <cstdlib>
-#include <cmath>
-
 using namespace std;
-
-void fun(double*, int);
-
-void fun(double* p, int n)
+class CBaseClass
 {
-    /***********************program*************************/
+public:
+	CBaseClass(int i, int j){
+		m_nX = i;
+		m_nY = j;
+	}
+	void MoveTo(int m_nXm, int m_nYm){
+		m_nX += m_nXm;
+		m_nY += m_nYm;
+		cout << "(" << m_nX << "," << m_nY << ")" << endl;
+	}
+	void ShowPos(){
+		cout << "(" << m_nX << "," << m_nY << ")" << endl;
+	}
+private:
+	int m_nX, m_nY;
+};
 
-    int i, j;
-    double temp;
-    for (i = 0; i < n-1; i++) {
-        for (j = i + 1; j < n; j++) {
-            if ((int)fabs(p[i]) / 10 % 1000 < (int)fabs(p[j]) / 10 % 1000) {
-                temp = p[i]; p[i] = p[j]; p[j] = temp;
-            }
-        }
-    }
-    /*************************END***************************/
-}
-
-int main(void)
+class CDerivedClass :public CBaseClass
 {
-    double a[10] = { -27888.52, 22177.83, 12496.14, -867.17, 29506.73, -18027.43, 32440.25, 91876.24,-18942.8, 10262.27 };
-    int i;
-    fun(a, 10);
+public:
+	CDerivedClass(int i, int j, int k, int l) :CBaseClass(i, j)
+	{
+		m_nXd = k;
+		m_nYd = l;
+	}
+	void ShowPos()
+	{
+		cout << "(" << m_nXd << "," << m_nYd << ")" << endl;
+	}
+	void func1()
+	{
+		MoveTo(3, 5);
+	}
+	void func2()
+	{
+		CBaseClass::ShowPos();
+	}
+private:
+	int m_nXd, m_nYd;
+};
 
-    cout << "排序结果：";
-    cout << fixed << setprecision(2);
-    for (i = 0; i < 10; i++) {
-        cout << setw(10) << a[i];
-    }
-    cout << endl;
-    
-    return 0;
-
+void main()
+{
+	CBaseClass b(1, 2);
+	b.ShowPos();
+	CDerivedClass d(3, 4, 5, 6);
+	d.func1();
+	d.ShowPos();
+	d.func2();
 }
