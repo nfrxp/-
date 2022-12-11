2ab7b8dae617c57d5878d0db8e7e49bf71f5abfd
小冉 <rjy716@qq.com>
2022-12-11 22:35:18 +0800

    代码

--- test-12-11/test-12-11/1.cpp
+++ test-12-11/test-12-11/1.cpp
@@ -18,13 +18,31 @@ public:
 
 };
 
-class sphere
+class sphere : public Body
 {
 public:
+	sphere(float x) :Body(x) {}
+	virtual float volume(void) { return 4.0f / 3 * PI * p * p * p; }
+};
 
+class cylinder :public Body
+{
+public:
+	cylinder(float x, float high) :Body(x) ,h(high) {}
+	virtual float volume(void) { return PI * p * p * h; }
+
+private:
+	float h;
 };
 
 int main() {
+	cube cube1(5.0);
+	cout << "正方体面积为：" << cube1.volume() << endl;
 
+	sphere sphere1(3.0);
+	cout << "球体的面积为：" << sphere1.volume() << endl;
+
+	cylinder cylinder1(3.0, 4.0);
+	cout << "圆柱体积为：" << cylinder1.volume() << endl;
 	return 0;
 }
