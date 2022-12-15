ffe4267e611cc020ef2380607b37b172f3d328fd
小冉 <rjy716@qq.com>
2022-12-15 21:35:00 +0800

    代码

--- test-12-14/test-12-14/1.c
+++ test-12-14/test-12-14/1.c
@@ -77,24 +77,51 @@
 //	return 0;
 //}
 
-#define _CRT_SECURE_NO_WARNINGS 1
-#include<stdio.h>
-#include<math.h>
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<math.h>
+//
+//int func(int a, int i) {
+//	int j = 1, sum = 0;
+//	for (j = 1; j <= i; j++) {
+//		if (i == 1) sum = a;
+//		else sum += a * pow(10, j-1);
+//	}
+//	return sum;
+//}
+//int main() {
+//	int a, Sn=0, i;
+//	scanf("%d", &a);
+//	for (i = 1; i <= 5; i++) {
+//		Sn += func(a, i);
+//	}
+//	printf("前五项之和为：%d", Sn);
+//	return 0;
+//}
 
-int func(int a, int i) {
-	int j = 1, sum = 0;
-	for (j = 1; j <= i; j++) {
-		if (i == 1) sum = a;
-		else sum += a * pow(10, j-1);
+
+//#include<stdio.h>
+//int main() {
+//	int sum = 20, j = 0;
+//	int i = sum;
+//	while(i>0) {
+//		i += j;
+//		sum += i/2;
+//		j = i % 2;
+//		i /= 2;
+//
+//	}
+//	printf("可以喝汽水 %d 瓶\n", sum);
+//	return 0;
+//}
+
+#include<stdio.h>			//断点
+int main()
+{
+	int i = 0;
+	for (i = 1; i < 10; i++) {
+ 		i++;		//若断点在循环中，此时可能会多次运行到此断点。
 	}
-	return sum;
-}
-int main() {
-	int a, Sn=0, i;
-	scanf("%d", &a);
-	for (i = 1; i <= 5; i++) {
-		Sn += func(a, i);
-	}
-	printf("前五项之和为：%d", Sn);
+	printf("%d", i);
 	return 0;
 }
