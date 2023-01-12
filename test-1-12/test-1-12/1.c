9c1c5ad7e3620f7c32b651184cd4d7943b1b8d12
小冉 <rjy716@qq.com>
2023-01-12 16:12:18 +0800

    代码

--- test-1-12/test-1-12/1.c
+++ test-1-12/test-1-12/1.c
@@ -33,7 +33,6 @@ void menu() {
 	printf("**  3.mul    4.div  **\n");
 	printf("**       0.exit     **\n");
 	printf("**********************\n");
-	printf("请选择计算类型:");
 }
 
 float add(float a, float b) {
@@ -52,34 +51,52 @@ int main() {
 	int input = 0;
 	float a = 0, b = 0;
 	//函数指针数组
+	//转移表
 	float (*func[5])(float a, float b) = { 0,add,sub,mul,div };
+
+	//指向函数指针数组的指针
+	float(*(*ffunc)[5])(float a, float b) = &func;
 	do {
 		menu();
 		scanf("%d", &input);
-		printf("请输入数据:");
-		switch (input) {
-		case 1:
-			scanf("%f %f", &a, &b);
-			printf("%f+%f=%f\n", a, b, (*func[input])(a, b));
-			break;
-		case 2:
-			scanf("%f %f", &a, &b);
-			printf(" %f + %f = %f\n", a, b, (*func[input])(a,b));
-			break;
-		case 3:
-			scanf("%f %f", &a, &b);
-			printf("%f*%f=%f\n", a, b, (*func[input])(a,b));
-			break;
-		case 4:
-			scanf("%f %f", &a, &b);
-			printf("%f/%f=%f\n", a, b, (*func[input])(a,b));
-			break;
-		case 0:
-			break;
-		default:
-			printf("输入错误,请重新输入!\n");
+		printf("请选择计算类型:");
+		if (input == 0) {
+			printf("退出!\n");
 			break;
 		}
+		else if (input >= 1 && input <= 4) {
+			printf("请输入数据:");
+			scanf("%f %f", &a, &b);
+			//printf("%f/%f=%f\n", a, b, (*func[input])(a, b));
+			printf("=%f\n", (*func[input])(a, b));
+			break;
+		}
+		else {
+			printf("请重新输入!\n");
+		}
+		//switch (input) {
+		//case 1:
+		//	scanf("%f %f", &a, &b);
+		//	printf("%f+%f=%f\n", a, b, (*func[input])(a, b));
+		//	break;
+		//case 2:
+		//	scanf("%f %f", &a, &b);
+		//	printf(" %f + %f = %f\n", a, b, (*func[input])(a,b));
+		//	break;
+		//case 3:
+		//	scanf("%f %f", &a, &b);
+		//	printf("%f*%f=%f\n", a, b, (*func[input])(a,b));
+		//	break;
+		//case 4:
+		//	scanf("%f %f", &a, &b);
+		//	printf("%f/%f=%f\n", a, b, (*func[input])(a,b));
+		//	break;
+		//case 0:
+		//	break;
+		//default:
+		//	printf("输入错误,请重新输入!\n");
+		//	break;
+		//}
 	} while (input);
 	return 0;
 }
