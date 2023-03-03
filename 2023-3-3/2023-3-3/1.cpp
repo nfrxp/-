f4b5378e15f0e03d29102f9101b996f9d8b8a323
小冉 <rjy716@qq.com>
2023-03-03 09:31:26 +0800

    代码

---
+++ 2023-3-3/2023-3-3/1.cpp
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+using namespace std;
+
+int main() {
+	char a[3] = "c";
+	char b[] = "a";
+	cout <<sizeof(a)<<endl;
+	cout << sizeof(b) << endl;
+	return 0;
+}
